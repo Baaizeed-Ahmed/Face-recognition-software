edbb456032ff412c58a2f4f5aed0605ed83b4b9e
baaizeed <baaizeed@DESKTOP-49GV060>
2024-08-26 09:59:37 +0100

    updates

--- EmployeeRecognitionApi/faceApi/obj/Debug/net8.0/faceApi.AssemblyInfo.cs
+++ EmployeeRecognitionApi/faceApi/obj/Debug/net8.0/faceApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("faceApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c9f2154d21dbe34df1aa2a391dd7c044bb71eaa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1b22e688fd6eb1ac1be5f69d445374ea4e8eb81")]
 [assembly: System.Reflection.AssemblyProductAttribute("faceApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("faceApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
